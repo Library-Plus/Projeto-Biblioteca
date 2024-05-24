8b70a8543adc6f5799da9213ae048026d523f3f8
unknown <patriciopng>
2024-05-24 17:06:47 -0300

    Mudança no index.html e style.css

--- index.php
+++ index.php
@@ -10,6 +10,34 @@
 			$msg = @$_GET['msg'];
 		?>
 		
+		
+
+		<header>
+			<div class="cabecalho">
+				<img class="logo" src="imagens/logo.png" alt="logo">
+			</div>
+			<div class="cabecalho">
+				<h1>  +biL </h1>
+			</div>
+			<div class="quebra">
+			</div>
+		</header>
+		
+		
+
+        <div class="form-login <?php if($erro == 1){ echo 'has-error';}?>">
+		<img class="user" src="imagens/user.png" alt="user" >
+			<form class="formulario" action="login_db.php" method="post">
+				<label for="usuario">Usuário:</label><br>
+				<input type="text" name="usuario" id="usuario" maxlength="50"><br><br>
+				
+				<label for="senha">Senha:</label><br>
+				<input type="password" name="senha" id="senha" maxlength="100"><br><br>
+
+				<button type="submit">Entrar</button>
+			</form>
+		</div>
+		<div class="div-erro">
 		<?php  if($erro) { ?>
 			<span class="erro">
 				<?php
@@ -21,30 +49,6 @@
 				?>
 			</span>
 		<?php  } ?>
-
-		<header>
-			<div class="cabecalho">
-				<img class="logo" src="imagens/logo.png" alt="logo">
-			</div>
-			<div class="cabecalho">
-				<h1>  Lib+ </h1>
-			</div>
-			<div class="quebra">
-			</div>
-		</header>
-		
-		<img class="user" src="imagens/user.png" alt="user" >
-		
-        <div class="login">
-			<form action="login_db.php" method="post">
-				<label for="usuario">Usuário:</label><br>
-				<input type="text" name="usuario" id="usuario" maxlength="50"><br><br>
-				
-				<label for="senha">Senha:</label><br>
-				<input type="password" name="senha" id="senha" maxlength="100"><br><br>
-
-				<button type="submit">Entrar</button>
-			</form>
 		</div>
     </body>
 </html>
