10081dd2f193b6abe7463be1cc81b5f7175ab9c2
unknown <patriciopng>
2024-05-24 17:56:36 -0300

    Mudancas no header da página, não finalizado

--- menu.php
+++ menu.php
@@ -1,28 +1,28 @@
-<body>
-<header>
-    <div class="cabecalho">
-        <img class="logo" src="imagens/logo.png" alt="logo">
-	</div>
-	<div class="cabecalho">
-		<h1>  Lib+ </h1>
-	</div>
-	<div class="quebra"> </div>
-</header>
 <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
-</body>
-<div class="menu">
-    <table>
-        <thead>
-            <tr>
-                <th><a href="listar_clientes.php">Clientes</a></th>
-                <th><a href="listar_vendedores.php">Vendedores</a></th>
-                <th><a href="listar_tipos.php">Tipos</a></th>
-                <th><a href="listar_livros.php">Livros</a></th>
-                <th><a href="listar_aluguel.php">Aluguel</a></th>
-                <th><a href="listar_usuarios.php">Usuários</a></th>
-                <th><a href="sair.php">Sair</a></th>
-            </tr>
-        </thead>
-    </table>
-</div>
+<body>
+    <header>
+        <div class="cabecalho">
+            <img class="logo" src="imagens/logo.png" alt="logo">
+	    </div>
+	    <div class="cabecalho">
+		    <h1>  Lib+ </h1>
+	    </div>
+	    <div class="quebra"> </div>
+        <div class="menu">
+            <table>
+                <thead>
+                    <tr>
+                        <th><a href="listar_clientes.php">Clientes</a></th>
+                        <th><a href="listar_vendedores.php">Vendedores</a></th>
+                        <th><a href="listar_tipos.php">Tipos</a></th>
+                        <th><a href="listar_livros.php">Livros</a></th>
+                        <th><a href="listar_aluguel.php">Aluguel</a></th>
+                        <th><a href="listar_usuarios.php">Usuários</a></th>
+                        <th><a href="sair.php">Sair</a></th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </header>
+</body>  
 <br><br>
