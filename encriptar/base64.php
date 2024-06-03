3172f528d8b37c177a433df2767e1a36a0833e62
patriciogabriel <sukiorxyz@gmail.com>
2024-06-02 19:40:31 -0700

    Removido arquivos não utilizados

--- encriptar/base64.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$valor = '123456';
-	echo base64_encode($valor);
-	
-	echo '<br>';
-	
-	echo base64_decode('MTIzNDU2');
-?>
--- encriptar/md5.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$valor = 'ESUCRI';
-	echo md5($valor);
-	// $resultado = md5($valor);
-	// echo md5($resultado);
-?>
